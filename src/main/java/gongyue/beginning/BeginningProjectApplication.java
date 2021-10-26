5feeaae98f975c10a69b150135644077b5bfcb06
龚越 <gongyue1@gongyuedeMacBook-Pro.local>
2021-10-26 17:45:07 +0800

    intit repo

---
+++ src/main/java/gongyue/beginning/BeginningProjectApplication.java
@@ -0,0 +1,13 @@
+package gongyue.beginning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginningProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeginningProjectApplication.class, args);
+    }
+
+}
