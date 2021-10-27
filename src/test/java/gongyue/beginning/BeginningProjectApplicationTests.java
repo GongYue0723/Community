f04f63a48d9341b8f26137e3d53fd2f6d567f439
GongYue0723 <1098879764@qq.com>
2021-10-27 18:13:15 +0800

    second commit

---
+++ src/test/java/gongyue/beginning/BeginningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package gongyue.beginning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginningProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
